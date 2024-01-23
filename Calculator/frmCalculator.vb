735ced6cf732504080ce91aa4b8f60c9cd234c2d
Scott Wegley <scbrwegley@protonmail.ch>
2024-01-23 15:38:53 -0800

    handle numeric button presses

--- Calculator/frmCalculator.vb
+++ Calculator/frmCalculator.vb
@@ -2,6 +2,9 @@
 
     ' Scott Wegley, CSC-300 Spring 2024, Homework #? - Build a Calculator
 
+    Public dblTermOne As Double
+    Public dblTermTwo As Double
+
     Private Sub txtDisplay_TextChanged(sender As Object, e As EventArgs) Handles txtDisplay.TextChanged
         For index = 0 To txtDisplay.TextLength - 1
             If Not IsNumeric(txtDisplay.Text.Substring(index, 1)) Then
@@ -10,4 +13,7 @@
         Next
     End Sub
 
+    Private Sub btnNumerics_Click(sender As Object, e As EventArgs) Handles btnOne.Click, btnTwo.Click, btnThree.Click, btnFour.Click, btnFive.Click, btnSix.Click, btnSeven.Click, btnEight.Click, btnNine.Click, btnZero.Click, btnDoubleZero.Click
+        txtDisplay.Text = txtDisplay.Text & CType(sender, Button).Text
+    End Sub
 End Class
