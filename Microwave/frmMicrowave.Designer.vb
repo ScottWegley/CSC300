31a160ad3fda792d241bee6c03414abf1ed6857b
Scott Wegley <scbrwegley@protonmail.ch>
2024-01-23 14:12:17 -0800

    Finished microwave design

---
+++ Microwave/frmMicrowave.Designer.vb
@@ -0,0 +1,222 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMicrowave
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnlDoor = New System.Windows.Forms.Panel()
+        Me.btnOne = New System.Windows.Forms.Button()
+        Me.btnThree = New System.Windows.Forms.Button()
+        Me.lblClock = New System.Windows.Forms.Label()
+        Me.btnTwo = New System.Windows.Forms.Button()
+        Me.pnlControls = New System.Windows.Forms.Panel()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnZero = New System.Windows.Forms.Button()
+        Me.btnNine = New System.Windows.Forms.Button()
+        Me.btnEight = New System.Windows.Forms.Button()
+        Me.btnSeven = New System.Windows.Forms.Button()
+        Me.btnSix = New System.Windows.Forms.Button()
+        Me.btnFive = New System.Windows.Forms.Button()
+        Me.btnFour = New System.Windows.Forms.Button()
+        Me.pnlControls.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnlDoor
+        '
+        Me.pnlDoor.BackColor = System.Drawing.Color.LightGray
+        Me.pnlDoor.Location = New System.Drawing.Point(13, 13)
+        Me.pnlDoor.Name = "pnlDoor"
+        Me.pnlDoor.Size = New System.Drawing.Size(530, 336)
+        Me.pnlDoor.TabIndex = 0
+        '
+        'btnOne
+        '
+        Me.btnOne.Location = New System.Drawing.Point(56, 128)
+        Me.btnOne.Name = "btnOne"
+        Me.btnOne.Size = New System.Drawing.Size(25, 20)
+        Me.btnOne.TabIndex = 2
+        Me.btnOne.Text = "1"
+        Me.btnOne.UseVisualStyleBackColor = True
+        '
+        'btnThree
+        '
+        Me.btnThree.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.btnThree.Location = New System.Drawing.Point(118, 128)
+        Me.btnThree.Name = "btnThree"
+        Me.btnThree.Size = New System.Drawing.Size(25, 20)
+        Me.btnThree.TabIndex = 3
+        Me.btnThree.Text = "3"
+        Me.btnThree.UseVisualStyleBackColor = True
+        '
+        'lblClock
+        '
+        Me.lblClock.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblClock.Location = New System.Drawing.Point(25, 25)
+        Me.lblClock.Margin = New System.Windows.Forms.Padding(25, 25, 25, 0)
+        Me.lblClock.Name = "lblClock"
+        Me.lblClock.Size = New System.Drawing.Size(150, 50)
+        Me.lblClock.TabIndex = 2
+        Me.lblClock.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnTwo
+        '
+        Me.btnTwo.Location = New System.Drawing.Point(87, 128)
+        Me.btnTwo.Name = "btnTwo"
+        Me.btnTwo.Size = New System.Drawing.Size(25, 20)
+        Me.btnTwo.TabIndex = 4
+        Me.btnTwo.Text = "2"
+        Me.btnTwo.UseVisualStyleBackColor = True
+        '
+        'pnlControls
+        '
+        Me.pnlControls.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.pnlControls.Controls.Add(Me.btnClear)
+        Me.pnlControls.Controls.Add(Me.btnStart)
+        Me.pnlControls.Controls.Add(Me.btnZero)
+        Me.pnlControls.Controls.Add(Me.btnNine)
+        Me.pnlControls.Controls.Add(Me.btnEight)
+        Me.pnlControls.Controls.Add(Me.btnSeven)
+        Me.pnlControls.Controls.Add(Me.btnSix)
+        Me.pnlControls.Controls.Add(Me.btnFive)
+        Me.pnlControls.Controls.Add(Me.btnFour)
+        Me.pnlControls.Controls.Add(Me.lblClock)
+        Me.pnlControls.Controls.Add(Me.btnThree)
+        Me.pnlControls.Controls.Add(Me.btnTwo)
+        Me.pnlControls.Controls.Add(Me.btnOne)
+        Me.pnlControls.Location = New System.Drawing.Point(572, 13)
+        Me.pnlControls.Name = "pnlControls"
+        Me.pnlControls.Size = New System.Drawing.Size(200, 336)
+        Me.pnlControls.TabIndex = 5
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(99, 297)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(100, 37)
+        Me.btnClear.TabIndex = 12
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(-2, 297)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(100, 37)
+        Me.btnStart.TabIndex = 6
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnZero
+        '
+        Me.btnZero.Location = New System.Drawing.Point(87, 206)
+        Me.btnZero.Name = "btnZero"
+        Me.btnZero.Size = New System.Drawing.Size(25, 20)
+        Me.btnZero.TabIndex = 11
+        Me.btnZero.Text = "0"
+        Me.btnZero.UseVisualStyleBackColor = True
+        '
+        'btnNine
+        '
+        Me.btnNine.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.btnNine.Location = New System.Drawing.Point(118, 180)
+        Me.btnNine.Name = "btnNine"
+        Me.btnNine.Size = New System.Drawing.Size(25, 20)
+        Me.btnNine.TabIndex = 9
+        Me.btnNine.Text = "9"
+        Me.btnNine.UseVisualStyleBackColor = True
+        '
+        'btnEight
+        '
+        Me.btnEight.Location = New System.Drawing.Point(87, 180)
+        Me.btnEight.Name = "btnEight"
+        Me.btnEight.Size = New System.Drawing.Size(25, 20)
+        Me.btnEight.TabIndex = 10
+        Me.btnEight.Text = "8"
+        Me.btnEight.UseVisualStyleBackColor = True
+        '
+        'btnSeven
+        '
+        Me.btnSeven.Location = New System.Drawing.Point(56, 180)
+        Me.btnSeven.Name = "btnSeven"
+        Me.btnSeven.Size = New System.Drawing.Size(25, 20)
+        Me.btnSeven.TabIndex = 8
+        Me.btnSeven.Text = "7"
+        Me.btnSeven.UseVisualStyleBackColor = True
+        '
+        'btnSix
+        '
+        Me.btnSix.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.btnSix.Location = New System.Drawing.Point(118, 154)
+        Me.btnSix.Name = "btnSix"
+        Me.btnSix.Size = New System.Drawing.Size(25, 20)
+        Me.btnSix.TabIndex = 6
+        Me.btnSix.Text = "6"
+        Me.btnSix.UseVisualStyleBackColor = True
+        '
+        'btnFive
+        '
+        Me.btnFive.Location = New System.Drawing.Point(87, 154)
+        Me.btnFive.Name = "btnFive"
+        Me.btnFive.Size = New System.Drawing.Size(25, 20)
+        Me.btnFive.TabIndex = 7
+        Me.btnFive.Text = "5"
+        Me.btnFive.UseVisualStyleBackColor = True
+        '
+        'btnFour
+        '
+        Me.btnFour.Location = New System.Drawing.Point(56, 154)
+        Me.btnFour.Name = "btnFour"
+        Me.btnFour.Size = New System.Drawing.Size(25, 20)
+        Me.btnFour.TabIndex = 5
+        Me.btnFour.Text = "4"
+        Me.btnFour.UseVisualStyleBackColor = True
+        '
+        'frmMicrowave
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 361)
+        Me.Controls.Add(Me.pnlControls)
+        Me.Controls.Add(Me.pnlDoor)
+        Me.Name = "frmMicrowave"
+        Me.Text = "Microwave Oven"
+        Me.pnlControls.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnlDoor As Panel
+    Friend WithEvents btnOne As Button
+    Friend WithEvents btnThree As Button
+    Friend WithEvents lblClock As Label
+    Friend WithEvents btnTwo As Button
+    Friend WithEvents pnlControls As Panel
+    Friend WithEvents btnZero As Button
+    Friend WithEvents btnNine As Button
+    Friend WithEvents btnEight As Button
+    Friend WithEvents btnSeven As Button
+    Friend WithEvents btnSix As Button
+    Friend WithEvents btnFive As Button
+    Friend WithEvents btnFour As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnStart As Button
+End Class
---
+++ Microwave/frmMicrowave.vb
@@ -0,0 +1,28 @@
+﻿Public Class frmMicrowave
+
+    Private Sub btnNum_Click(sender As Object, e As EventArgs) Handles btnOne.Click, btnTwo.Click, btnThree.Click, btnFour.Click, btnFive.Click, btnSix.Click, btnSeven.Click, btnEight.Click, btnNine.Click, btnZero.Click
+        If pnlDoor.BackColor = Color.LightGreen Then GoTo Quit
+        Dim intToAdd As Integer = CInt(CType(sender, Button).Text)
+        lblClock.Text = ""
+        lblClock.Tag = lblClock.Tag & intToAdd
+        If lblClock.Tag.ToString().Length = 5 Then
+            lblClock.Tag = lblClock.Tag.ToString().Substring(1)
+        End If
+        If lblClock.Tag.ToString().Length > 2 Then
+            lblClock.Text = Strings.Left(lblClock.Tag.ToString(), lblClock.Tag.ToString().Length - 2) & ":"
+        End If
+        lblClock.Text = lblClock.Text & Strings.Right(lblClock.Tag.ToString(), 2)
+Quit:
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        lblClock.Tag = ""
+        lblClock.Text = ""
+        pnlDoor.BackColor = Color.LightGray
+    End Sub
+
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        lblClock.Text = "Running"
+        pnlDoor.BackColor = Color.LightGreen
+    End Sub
+End Class
