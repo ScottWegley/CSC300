a592d2becf583c4ee45b7e07d8cecba7dd8ec55b
Scott Wegley <scbrwegley@protonmail.ch>
2024-01-23 14:38:48 -0800

    renamed form, did text entry validation

---
+++ Calculator/frmCalculator.vb
@@ -0,0 +1,12 @@
+﻿Public Class frmCalculator
+
+    ' Scott Wegley, CSC-300 Spring 2024, Homework #? - Build a Calculator
+
+    Private Sub txtDisplay_TextChanged(sender As Object, e As EventArgs) Handles txtDisplay.TextChanged
+        For index = 0 To txtDisplay.TextLength - 1
+            If Not IsNumeric(txtDisplay.Text.Substring(index, 1)) Then
+                txtDisplay.Text = txtDisplay.Text.Replace(txtDisplay.Text.Substring(index, 1), "")
+            End If
+        Next
+    End Sub
+End Class
