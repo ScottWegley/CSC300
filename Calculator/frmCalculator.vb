0ae101cc07f532ee2cbbef089b4e0ff4df2c2ff4
Scott Wegley <scbrwegley@protonmail.ch>
2024-01-25 15:04:20 -0800

    added handling for decimal button and key

--- Calculator/frmCalculator.vb
+++ Calculator/frmCalculator.vb
@@ -1,19 +1,46 @@
-﻿Public Class frmCalculator
+﻿Option Strict On
+Option Explicit On
+Option Infer Off
+
+Public Class frmCalculator
 
     ' Scott Wegley, CSC-300 Spring 2024, Homework #? - Build a Calculator
 
     Public dblTermOne As Double
     Public dblTermTwo As Double
 
+    Public boolFirstDecimal As Boolean = False
+    Public boolSecondDecimal As Boolean = False
+
+    Public boolStoreFirst As Boolean = True
+
     Private Sub txtDisplay_TextChanged(sender As Object, e As EventArgs) Handles txtDisplay.TextChanged
-        For index = 0 To txtDisplay.TextLength - 1
-            If Not IsNumeric(txtDisplay.Text.Substring(index, 1)) Then
-                txtDisplay.Text = txtDisplay.Text.Replace(txtDisplay.Text.Substring(index, 1), "")
+        If Not txtDisplay.Text.IndexOf(".") = -1 Then
+            If boolStoreFirst Then boolFirstDecimal = True Else boolSecondDecimal = True
+        End If
+        For I As Integer = 0 To txtDisplay.TextLength - 1
+            If Not (IsNumeric(txtDisplay.Text.Substring(I, 1)) Or txtDisplay.Text.Substring(I, 1) = ".") Then
+                txtDisplay.Text = txtDisplay.Text.Replace(txtDisplay.Text.Substring(I, 1), "")
             End If
         Next
     End Sub
 
     Private Sub btnNumerics_Click(sender As Object, e As EventArgs) Handles btnOne.Click, btnTwo.Click, btnThree.Click, btnFour.Click, btnFive.Click, btnSix.Click, btnSeven.Click, btnEight.Click, btnNine.Click, btnZero.Click, btnDoubleZero.Click
         txtDisplay.Text = txtDisplay.Text & CType(sender, Button).Text
+    End Sub
+
+    'Private Sub btnOperations_Click(sender As Object, e As EventArgs) Handles btnPlus.Click, btnMinus.Click, btnMultiply.Click, btnDivide.Click, btnEquals.Click
+
+    'End Sub
+
+    Private Sub btnDecimal_Click(sender As Object, e As EventArgs) Handles btnDecimal.Click
+        If If(boolStoreFirst, Not boolFirstDecimal, Not boolSecondDecimal) Then
+            txtDisplay.Text = txtDisplay.Text & "."
+            If boolStoreFirst Then boolFirstDecimal = True Else boolSecondDecimal = True
+        End If
+    End Sub
+
+    Private Sub frmCalculator_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        txtDisplay.Focus()
     End Sub
 End Class
